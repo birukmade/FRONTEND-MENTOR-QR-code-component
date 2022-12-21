0d91b04f41f34eeca56c8c0043da1235974690af
Biruk Kalewold <birukkalewoldasrat@gmail.com>
2022-12-21 23:04:11 +0300

    customized tailwind css configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        White: "hsl(0, 0%, 100%)",
+        LightGray: "hsl(212, 45%, 89%)",
+        GrayishBlue: "hsl(220, 15%, 55%)",
+        DarkBlue: "hsl(218, 44%, 22%)",
+      },
+    },
+    fontFamily: {
+      outfit: ["Outfit", "sans-serif"],
+    },
   },
   plugins: [],
 };
